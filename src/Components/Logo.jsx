a693d6082832244e5259d9c81ee52b4b7df3f2d9
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2023-07-04 04:04:14 -0300

    Updated Logo component from text to an SVG

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,7 +1,12 @@
 import styles from "./Logo.module.css";
+import { ReactComponent as Refrigerios } from "../assets/refrigerios.svg";
 
 const Logo = () => {
-  return <h1 className={styles.logo}>Refrigerios</h1>;
+  return (
+    <div className={styles["logo-container"]}>
+      <Refrigerios className={styles.logo} />
+    </div>
+  );
 };
 
 export default Logo;
