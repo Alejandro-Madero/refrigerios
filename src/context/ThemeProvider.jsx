732a68c6f7a327183d68d9f95fd7b93fb5c4d9b0
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2023-07-23 20:54:42 -0300

    Saves prefered theme on localStorage and updated Header component styles

--- src/context/ThemeProvider.jsx
+++ src/context/ThemeProvider.jsx
@@ -3,9 +3,10 @@ import { createContext, useState } from "react";
 export const ThemeContext = createContext({ theme: "dark" });
 
 export default function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "dark");
 
   const toggleTheme = () => {
+    localStorage.setItem("theme", theme === "dark" ? "light" : "dark");
     setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
   };
 
